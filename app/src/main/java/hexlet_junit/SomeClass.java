d7154977a4e9d8bf1a454ae61f31b15ccc16544e
Denis <denzh@mail.ru>
2023-07-16 00:57:19 +0300

    new project

---
+++ app/src/main/java/hexlet_junit/SomeClass.java
@@ -0,0 +1,7 @@
+package hexlet_junit;
+
+class SomeClass {
+  public static int sum(int a, int b) {
+    return a + b;
+  }
+}
---
+++ app/src/test/java/hexlet_junit/SomeClassTest.java
@@ -0,0 +1,14 @@
+package hexlet_junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class SomeClassTest {
+
+  @Test
+  public void testSum() {
+    var expected = 5;
+    var actual = SomeClass.sum(2, 3);
+    assertEquals(expected, actual);
+  }
+}
